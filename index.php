29e4f7b80dd8efc4b6ec6f582ccbb428bf6930b4
joskar162 <joshuakarani60@gmail.com>
2025-11-25 23:45:45 +0300

    included config file and check if user is loged in

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?php
 // TODO: Include config file
-
+require_once(__DIR__ . '/config.php');
 
 // TODO: Check if user is logged in
 // If logged in, redirect to appropriate dashboard based on role
 // If not logged in, redirect to login page
+
+if (isLoggedIn()) {
+    $role = $_SESSION['role'];
+    if ($role === 'student') {
+        header("Location: dashboard_student.php");
+        exit();
+    } elseif ($role === 'lecturer') {
+        header("Location: dashboard_lecturer.php");
+        exit();
+    }
+} else {
+    header("Location: login.php");
+    exit();
+}
 
 ?>
