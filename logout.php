31559b012a0180bc55feacafdd0a479fbaec7fe3
joskar162 <joshuakarani60@gmail.com>
2025-11-26 00:00:56 +0300

    added the config file , destory session and redirected to the login page

--- logout.php
+++ logout.php
@@ -1,10 +1,16 @@
 <?php
 // TODO: Include config file
+require_once(__DIR__ . '/config.php');
 
 
 // TODO: Destroy session
+session_start();
+session_unset();
+session_destroy();
 
 
 // TODO: Redirect to login page
+header("Location: login.php");
+exit();
 
 ?>
