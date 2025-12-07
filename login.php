24c773b688821553cf3754f2334dc627dc914858
joskar162 <joshuakarani60@gmail.com>
2025-12-07 23:27:52 +0300

    add breadk in the login box

--- login.php
+++ login.php
@@ -99,9 +99,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <form method="POST" action="">
                 <!-- Add form fields here -->
                 <label for="username">Username:</label>
-                <input type="text" id="username" name="username" required>
+                <input type="text" id="username" name="username" required><br>
                 <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
+                <input type="password" id="password" name="password" required><br>
                 <button type="submit" name="login">Login</button>
             </form>
             <br>
